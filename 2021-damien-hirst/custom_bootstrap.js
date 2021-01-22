24e0639b911c87791a1e0517a780e18fc42650a6
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-01-22 18:23:41 +0000

    comment main changes

--- 2021-damien-hirst/custom_bootstrap.js
+++ 2021-damien-hirst/custom_bootstrap.js
@@ -1,13 +1,13 @@
 console.log('custom_bootstrap.js triggered...');
 
 // General page styling
-setTimeout(function() {
-  const main = document.getElementsByTagName('main');
-  if (main) {
-    main[0].style.padding = "0";
-    console.log(main[0]);
-  }
-}, 2000);
+// setTimeout(function() {
+//   const main = document.getElementsByTagName('main');
+//   if (main) {
+//     main[0].style.padding = "0";
+//     console.log(main[0]);
+//   }
+// }, 2000);
 
 
 // Custom code blocks
