7b29584567f0fa594d5d663b01ee4d845843d008
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-01-22 18:20:08 +0000

    wrap in setTimeouts

--- 2021-damien-hirst/custom_bootstrap.js
+++ 2021-damien-hirst/custom_bootstrap.js
@@ -1,8 +1,12 @@
 console.log('custom_bootstrap.js triggered...');
 
 // General page styling
-const main = document.getElementsByTagName('main');
-main[0].style.padding = "0";
+setTimeout(function() {
+  const main = document.getElementsByTagName('main');
+  main[0].style.padding = "0";
+  console.log(main[0]);
+}, 2000);
+
 
 // Custom code blocks
 setTimeout(function() {
