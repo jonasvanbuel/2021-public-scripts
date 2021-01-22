7535f67bca3901ad0f672540efeb495b257c8265
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-01-22 18:11:36 +0000

    Disable padding code blocks

--- 2021-damien-hirst/custom_bootstrap.js
+++ 2021-damien-hirst/custom_bootstrap.js
@@ -1,3 +1,14 @@
 console.log('custom_bootstrap.js triggered...');
+
+// General page styling
 const main = document.getElementsByTagName('main');
 main[0].style.padding = "0";
+
+// Custom code blocks
+const sqsBlockCode = document.getElementsByClassName('sqs-block-code');
+if (sqsBlockCode) {
+  sqsBlockCode.forEach(blockCode => {
+    console.log(blockCode);
+    blockCode.parentNode.style.padding = "0";
+  })
+}
