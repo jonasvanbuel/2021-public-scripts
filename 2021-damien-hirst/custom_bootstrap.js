aef22736c329ee0de716203f7576f80a778251e3
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-01-22 18:40:12 +0000

    resize slideshows

--- 2021-damien-hirst/custom_bootstrap.js
+++ 2021-damien-hirst/custom_bootstrap.js
@@ -9,22 +9,55 @@ console.log('custom_bootstrap.js triggered...');
 //   }
 // }, 2000);
 
+const windowWith = window.with;
 
-// Custom code blocks
 window.onload = () => {
   console.log('window loaded...');
+
+  // Custom code blocks
   const sqsBlockCode = document.getElementsByClassName('sqs-block-code');
   if (sqsBlockCode) {
     sqsBlockCode.forEach(blockCode => {
       blockCode.parentNode.style.padding = "0";
       console.log(blockCode);
     })
-  }
+  };
+
+  // Scale fullscreen slideshows
+  const galleryFullscreenSlideshow =  document.getElementsByClassName('gallery-fullscreen-slideshow');
+  galleryFullscreenSlideshow.forEach(slideshow => {
+    if (windowWith <= 576) {
+      slideshow.style.height = "320px";
+    };
+    if (windowWith <= 992) {
+      slideshow.style.height = "480px";
+    }
+    if (windowWith <= 1200) {
+      slideshow.style.height = "600px";
+    }
+    if (windowWith > 1200) {
+      slideshow.style.height = "700px";
+    }
+  })
 }
 
 
-// setTimeout(function() {
+// RESPONSIVE TEMPLATE
+// Extra small devices (portrait phones, less than 576px)
+// No media query since this is the default in Bootstrap
 
-// }, 2000);
+// Small devices (landscape phones, 576px and up)
+// @media (min-width: 576px) {
+// }
 
+// Medium devices (tablets, 768px and up)
+// @media (min-width: 768px) {
+// }
 
+// Large devices (desktops, 992px and up)
+// @media (min-width: 992px) {
+// }
+
+// Extra large devices (large desktops, 1200px and up)
+// @media (min-width: 1200px) {
+// }
