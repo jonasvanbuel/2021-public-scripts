fd79a2eeb9949e4f5b733fef0675c91f34b4e396
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-01-26 16:32:02 +0000

    mapboxgl_instance.js

---
+++ 2021-damien-hirst/mapboxgl_instance.js
@@ -0,0 +1,137 @@
+console.log('mapboxgl_instance.js triggered...');
+
+// TODO: Hide accessToken
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9uYXN2YW5idWVsIiwiYSI6ImNranR5NjZ6ZzQzM3YzMHFvNWUyMzhsNHQifQ.FQsG7QWSzTDUmYskQEAuLQ';
+
+const centerLocation = [9.845783, 46.493951];
+
+const geojson = {
+  type: 'exhibition-locations',
+  features: [{
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [9.836229555003456,46.48338716026936]
+    },
+    properties: {
+      title: 'Forum Paracelsus,<br>St. Moritz Bad',
+      address: 'Plazza Paracelsus 2,<br>7500 St. Moritz',
+      locationUrl: 'https://goo.gl/maps/e6ZckP1XyYYJsd1j6',
+      moreInfoUrl: '/uwr2m4-forum',
+      markerColor: '#7a2f8a'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [9.837382652620704,46.49037730561599]
+    },
+    properties: {
+      title: 'Two Figures with a Drum, 2013',
+      address: 'Via Ludains 3, 7500 St. Moritz',
+      locationUrl: 'https://goo.gl/maps/3SoqqpMZM3zGY2cm8',
+      moreInfoUrl: '/uwr2m4-forum',
+      markerColor: '#f36f23'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [9.843045172309075,46.495743903473056]
+    },
+    properties: {
+      title: 'The Monk, 2014',
+      address: 'Lake St Moritz, 7500 St. Moritz',
+      locationUrl: 'https://goo.gl/maps/2Lu9nDnGgXTbeDEN9',
+      moreInfoUrl: '/uwr2m4-forum',
+      markerColor: '#5aa447'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [9.848247537562429,46.497075969819974]
+    },
+    properties: {
+      title: 'Temple, 2008',
+      address: 'Via Dimlej 6, 7500 St. Moritz',
+      locationUrl: 'https://goo.gl/maps/3SoqqpMZM3zGY2cm8',
+      moreInfoUrl: '/uwr2m4-forum',
+      markerColor: '#f6ee4e'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [9.839961459203437,46.49840041968736]
+    },
+    properties: {
+      title: 'Protestant Church, St. Moritz Dorf',
+      address: 'Via Veglia 12, 7500 St. Moritz',
+      locationUrl: 'https://goo.gl/maps/TQvKjJiNUzM64WN79',
+      moreInfoUrl: '/uwr2m4-forum',
+      markerColor: '#bf64a8'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [9.839864288026092,46.49846347218508]
+    },
+    properties: {
+      title: 'Proteus, 2012',
+      address: 'Via Veglia 12, 7500 St. Moritz',
+      locationUrl: 'https://goo.gl/maps/TQvKjJiNUzM64WN79',
+      moreInfoUrl: '/uwr2m4-forum',
+      markerColor: '#e5232c'
+    }
+  }]
+};
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/jonasvanbuel/ckju13q210vxj19ofdvq8so5l', // stylesheet location
+    center: centerLocation, // starting position [lng, lat]
+    zoom: 13
+});
+
+map.fitBounds([
+  [9.836229555003456,46.48338716026936],
+  [9.839961459203437,46.49840041968736]
+],
+{
+  padding: {top: 60, right: 100, bottom: 60, left: 60},
+  offset: [-20, 0]
+})
+
+// add markers to map
+geojson.features.forEach(function(marker) {
+
+  // Marker element
+  // const markerEl = document.createElement('div');
+  // markerEl.className = 'marker';
+
+  // Circle element
+  const dotMarkerEl = document.createElement('div');
+  dotMarkerEl.className = "dot-marker";
+  dotMarkerEl.style = `background-color:${marker.properties.markerColor}`;
+
+  // Popup element
+  const popupContent = `
+    <h3 style="color:inherit;margin-bottom:12px;margin-top:0">${marker.properties.title}</h3>
+    <a href="${marker.properties.locationUrl}" target="_blank" style="margin-bottom:12px">${marker.properties.address}</a>
+    <a class="find-out-more" href="${marker.properties.moreInfoUrl}">Find out more</a>
+  `;
+
+  // make a marker for each feature and add to the map
+  new mapboxgl.Marker(dotMarkerEl)
+    .setLngLat(marker.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup() // add popups
+                          .setHTML(popupContent))
+    .addTo(map)
+});
